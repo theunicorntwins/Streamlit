1e799b46556ee6cfce65b6667f37cd3527b33ed8
theunicorntwins <123466747+theunicorntwins@users.noreply.github.com>
2023-02-18 21:27:08 +0700

    Add files via upload

---
+++ SpellCorrection/SpellCorrection.py
@@ -0,0 +1,22 @@
+from nltk import word_tokenize
+from autocorrect import Speller
+import string
+import streamlit as st
+
+st.title("Spell Correction")
+st.write("""
+## A tool that helps to fix your error vocabulary?
+""")
+
+sentence = st.text_input('Your text is: ')
+
+tokens = word_tokenize(sentence)
+
+spell = Speller(lang='en')
+
+def correct_spelling(tokens):
+    sentence_corrected = ' '.join([spell(word) for word in tokens])
+    return sentence_corrected
+
+st.write(correct_spelling(tokens))
+
